6cee0de54f310fe9a535abafb7c46ff6a367acad
Ciprian Burca <ciprian.burca@outlook.com>
2021-12-02 08:10:49 +0200

    Day 02

---
+++ day02/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let raw_input = std::fs::read_to_string("src/input").expect("Error reading the file!");
+    let directions = raw_input
+        .lines()
+        .map(|l| {
+            let mut chunks = l.split(' ');
+            return (
+                chunks.next().unwrap(),
+                chunks.next().unwrap().parse::<usize>().unwrap(),
+            );
+        })
+        .collect::<Vec<_>>();
+
+    let pos1 = directions
+        .iter()
+        .fold((0, 0), |(x, y), (dir, dist)| match dir {
+            &"forward" => (x + dist, y),
+            &"down" => (x, y + dist),
+            &"up" => (x, y - dist),
+            _ => panic!("Unknown direction!"),
+        });
+    println!("Part1: {}", pos1.0 * pos1.1);
+
+    let pos2 = directions
+        .iter()
+        .fold((0, 0, 0), |(x, y, a), (dir, dist)| match dir {
+            &"forward" => (x + dist, y + dist * a, a),
+            &"down" => (x, y, a + dist),
+            &"up" => (x, y, a - dist),
+            _ => panic!("Unknown direction!"),
+        });
+    println!("Part1: {}", pos2.0 * pos2.1);
+}
