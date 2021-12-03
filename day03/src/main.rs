053d1c9b2cdda0ff474f7d3e25da2dbc65ee06f4
Ciprian Burca <ciprian.burca@outlook.com>
2021-12-03 18:02:54 +0200

    Code refactoring

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,7 +4,7 @@ fn parse_input(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|l| l.chars().collect()).collect()
 }
 
-fn most_common_bits(grid: Vec<Vec<char>>, on_tie_use: char) -> Vec<char> {
+fn most_common_bits(grid: Vec<Vec<char>>) -> Vec<char> {
     let flipped: Vec<Vec<char>> = (0..grid[0].len())
         .map(|i| grid.iter().map(|c| c[i]).collect())
         .collect();
@@ -12,16 +12,16 @@ fn most_common_bits(grid: Vec<Vec<char>>, on_tie_use: char) -> Vec<char> {
     flipped
         .into_iter()
         .map(|a| {
-            let c = a.into_iter().collect::<Counter<_>>().most_common();
-            if c.len() == 2 && c[0].1 == c[1].1 {
-                return on_tie_use;
-            }
-            c[0].0
+            let c = a
+                .into_iter()
+                .collect::<Counter<_>>()
+                .most_common_tiebreaker(|&a, &b| b.cmp(&a)); //On tie order ('1', '0')
+            c.iter().next().unwrap().0
         })
         .collect()
 }
 
-fn least_common_bits(grid: Vec<Vec<char>>, on_tie_use: char) -> Vec<char> {
+fn least_common_bits(grid: Vec<Vec<char>>) -> Vec<char> {
     let flipped: Vec<Vec<char>> = (0..grid[0].len())
         .map(|i| grid.iter().map(|c| c[i]).collect())
         .collect();
@@ -29,62 +29,46 @@ fn least_common_bits(grid: Vec<Vec<char>>, on_tie_use: char) -> Vec<char> {
     flipped
         .into_iter()
         .map(|a| {
-            let c = a.into_iter().collect::<Counter<_>>().most_common();
-            if c.len() == 2 && c[0].1 == c[1].1 {
-                return on_tie_use;
-            } else if c.len() == 1 {
-                return c[0].0;
-            }
-            c[1].0
+            let c = a
+                .into_iter()
+                .collect::<Counter<_>>()
+                .most_common_tiebreaker(|&a, &b| b.cmp(&a)); //On tie order ('1', '0')
+            c.iter().last().unwrap().0
         })
         .collect()
+}
+
+fn calculate_rating(grid: Vec<Vec<char>>,  filter: fn(Vec<Vec<char>>) -> Vec<char>) -> i32 {
+    let mut rating = grid.clone();
+    for i in 0..rating[0].len() {
+        if rating.len() == 1 {
+            break;
+        }
+        let distrib = filter(rating.clone());
+        let tmp: Vec<Vec<char>> = rating
+            .clone()
+            .into_iter()
+            .filter(|l| l[i] == distrib[i])
+            .collect();
+        if tmp.len() == 0 {
+            continue;
+        }
+        rating = tmp;
+    }
+     i32::from_str_radix(&rating[0].iter().collect::<String>(), 2).unwrap()
 }
 
 fn power_consumption(grid: Vec<Vec<char>>) -> i32 {
-    let most_common = most_common_bits(grid.clone(), '1');
-    let least_common = least_common_bits(grid.clone(), '0');
+    let most_common = most_common_bits(grid.clone());
+    let least_common = least_common_bits(grid.clone());
 
     i32::from_str_radix(&most_common.iter().collect::<String>(), 2).unwrap()
         * i32::from_str_radix(&least_common.iter().collect::<String>(), 2).unwrap()
 }
 
 fn life_support_rating(grid: Vec<Vec<char>>) -> i32 {
-    let mut oxygen_rating = grid.clone();
-    for i in 0..oxygen_rating[0].len() {
-        if oxygen_rating.len() == 1 {
-            break;
-        }
-        let most_common = most_common_bits(oxygen_rating.clone(), '1');
-        let tmp: Vec<Vec<char>> = oxygen_rating
-            .clone()
-            .into_iter()
-            .filter(|l| l[i] == most_common[i])
-            .collect();
-        if tmp.len() == 0 {
-            continue;
-        }
-        oxygen_rating = tmp;
-    }
-
-    let mut co2_rating = grid.clone();
-    for i in 0..co2_rating[0].len() {
-        if co2_rating.len() == 1 {
-            break;
-        }
-        let least_common = least_common_bits(co2_rating.clone(), '0');
-        let tmp: Vec<Vec<char>> = co2_rating
-            .clone()
-            .into_iter()
-            .filter(|l| l[i] == least_common[i])
-            .collect();
-        if tmp.len() == 0 {
-            continue;
-        }
-        co2_rating = tmp;
-    }
-
-    i32::from_str_radix(&oxygen_rating[0].iter().collect::<String>(), 2).unwrap()
-        * i32::from_str_radix(&co2_rating[0].iter().collect::<String>(), 2).unwrap()
+    calculate_rating(grid.clone(), most_common_bits)
+        * calculate_rating(grid.clone(), least_common_bits)
 }
 
 fn main() {
@@ -104,7 +88,7 @@ mod tests {
 
     #[test]
     fn test_power_consumption() {
-        let test_input ="00100
+        let test_input = "00100
 11110
 10110
 10111
@@ -120,9 +104,9 @@ mod tests {
         assert_eq!(power_consumption(grid), 198);
     }
 
-        #[test]
+    #[test]
     fn test_life_support_rating() {
-        let test_input ="00100
+        let test_input = "00100
 11110
 10110
 10111
